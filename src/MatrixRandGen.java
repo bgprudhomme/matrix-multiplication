fa85892bca3d21bf1050e7ada898f3ad8831bf8d
bgprudhomme <bprudhomme@gradcenter.cuny.edu>
2023-09-20 13:49:44 -0400

    Test matrices up to 1024x1024

---
+++ src/MatrixRandGen.java
@@ -0,0 +1,40 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class MatrixRandGen {
+    public static void main(String[] args) {
+        File f;
+        BufferedWriter w;
+        int min = -100;
+        int max = 100;
+
+        int[] sizes = {32, 64, 128, 256, 512, 1024};
+
+        for(int s : sizes){
+            try {
+                System.out.println("Size: " + s + "x" + s);
+                for(int x=0; x<10; x++){
+                    f = new File("../test/" + s + "x" + s + "/" + x + ".txt");
+                    
+                        w = new BufferedWriter(new FileWriter(f, true));
+                        w.write(s + "\n");
+                        for(int i=0; i<s; i++){
+                            for(int j=0; j<s; j++){
+                                w.write(ThreadLocalRandom.current().nextInt(min, max + 1) + " ");
+                            }
+                            w.write("\n");
+                        }
+                    
+                    System.out.println("File created: ../test/" + s + "x" + s + "/" + x + ".txt");
+                    w.close();
+                }
+                
+            } catch (IOException e){
+                    System.out.println("IOException occurred");
+                }
+        }
+    }
+}
