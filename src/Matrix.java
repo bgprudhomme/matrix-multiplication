e12bbf909964c8881a181449add583eec629e62f
bgprudhomme <bprudhomme@gradcenter.cuny.edu>
2023-09-20 12:56:31 -0400

    Initial Strassen implementation

--- src/Matrix.java
+++ src/Matrix.java
@@ -31,6 +31,32 @@ public class Matrix {
         }
     }
 
+    // Constructor for matrix composed from 2x2 partitioning of smaller matrices
+    public Matrix (Matrix c11, Matrix c12, Matrix c21, Matrix c22) {
+        this.rows = c11.rows() + c21.rows();
+        this.cols = c11.cols() + c12.cols();
+        entries = new ArrayList<ArrayList<Integer>>();
+
+        for(int i=0; i<c11.rows(); i++){
+            entries.add(new ArrayList<Integer>());
+            for(int j=0; j<c11.cols(); j++){
+                ((ArrayList<Integer>) entries.get(i)).add(c11.get(i, j));
+            }
+            for(int j=0; j<c12.cols(); j++){
+                ((ArrayList<Integer>) entries.get(i)).add(c12.get(i, j));
+            }
+        }
+        for(int i=0; i<c21.rows(); i++){
+            entries.add(new ArrayList<Integer>());
+            for(int j=0; j<c21.cols(); j++){
+                ((ArrayList<Integer>) entries.get(i+c11.rows())).add(c21.get(i, j));
+            }
+            for(int j=0; j<c22.cols(); j++){
+                ((ArrayList<Integer>) entries.get(i+c11.rows())).add(c22.get(i, j));
+            }
+        }
+    }
+
     public int rows(){
         return rows;
     }
@@ -49,6 +75,16 @@ public class Matrix {
 
     public void add(int row, int col, int add){
         ((ArrayList<Integer>) entries.get(row)).set(col, get(row, col) + add);
+    }
+
+    public Matrix partition(int startRow, int endRow, int startCol, int endCol){
+        Matrix result = new Matrix(endRow-startRow, endCol-startCol);
+        for(int i=0; i<endRow-startRow; i++){
+            for(int j=0; j<endCol-startCol; j++){
+                result.set(i, j, this.get(startRow+i, startCol+j));
+            }
+        }
+        return result;
     }
 
     public String toString(){
--- src/MatrixMultiplication.java
+++ src/MatrixMultiplication.java
@@ -4,11 +4,11 @@ import java.io.FileNotFoundException;
 
 public class MatrixMultiplication {
     public static void main(String[] args){
-        Matrix a, b;
+        Matrix a, b, c1, c2;
 
-        File f = new File("../test/1.txt");
+        File file = new File("../test/1.txt");
         try {
-            Scanner in = new Scanner(f);
+            Scanner in = new Scanner(file);
         
             int size = in.nextInt();
 
@@ -28,29 +28,110 @@ public class MatrixMultiplication {
 
             in.close();
 
-            System.out.println(a);
-            System.out.println(b);
+            c1 = multiplyDirect(a, b);
+            c2 = multiplyStrassen(a, b);
 
-            System.out.println(multiplyDirect(a, b));
+            System.out.println("Direct:\n" + c1);
 
-        } catch(FileNotFoundException e){
+            System.out.println("Strassen:\n" + c2);
+            
+            // System.out.println("a11:\n" + a.partition(0, a.rows()/2, 0, a.cols()/2));
+            // System.out.println("a12:\n" + a.partition(0, a.rows()/2, a.cols()/2, a.cols()));
+            // System.out.println("a21:\n" + a.partition(a.rows()/2, a.rows(), 0, a.cols()/2));
+            // System.out.println("a22:\n" + a.partition(a.rows()/2, a.rows(), a.cols()/2, a.cols()));
+
+        } catch(FileNotFoundException ex){
             System.out.println("File not found");
         }
     }
 
-    public static void readFromFile(String path, Matrix a, Matrix b){
-        
+    public static Matrix add(Matrix a, Matrix b){
+        Matrix c = new Matrix(a.rows(), a.cols());
+        for(int i=0; i<a.rows(); i++){
+            for(int j=0; j<b.cols(); j++){
+                c.set(i, j, a.get(i, j) + b.get(i, j));
+            }
+        }
+        return c;
+    }
+
+    public static Matrix subtract(Matrix a, Matrix b){
+        Matrix c = new Matrix(a.rows(), a.cols());
+        for(int i=0; i<a.rows(); i++){
+            for(int j=0; j<b.cols(); j++){
+                c.set(i, j, a.get(i, j) - b.get(i, j));
+            }
+        }
+        return c;
     }
 
     public static Matrix multiplyDirect(Matrix a, Matrix b){
-        Matrix result = new Matrix(a.rows(), b.cols());
+        Matrix c = new Matrix(a.rows(), b.cols());
+
         for(int i=0; i<a.rows(); i++){
             for(int j=0; j<b.cols(); j++){
                 for(int k=0; k<a.cols(); k++){
-                    result.add(i, j, a.get(i, k)*b.get(k, j));
+                    c.add(i, j, a.get(i, k)*b.get(k, j));
                 }
             }
         }
-        return result;
+        return c;
     }
+
+    public static Matrix multiplyStrassen(Matrix a, Matrix b){
+        Matrix c = new Matrix(a.rows(), a.cols());
+        if(a.rows() == 1){
+            c.add(0, 0, a.get(0, 0) * b.get(0, 0));
+        }
+        else {
+            // Step 1
+            Matrix a11 = a.partition(0, a.rows()/2, 0, a.cols()/2);
+            Matrix a12 = a.partition(0, a.rows()/2, a.cols()/2, a.cols());
+            Matrix a21 = a.partition(a.rows()/2, a.rows(), 0, a.cols()/2);
+            Matrix a22 = a.partition(a.rows()/2, a.rows(), a.cols()/2, a.cols());
+            Matrix b11 = b.partition(0, b.rows()/2, 0, b.cols()/2);
+            Matrix b12 = b.partition(0, b.rows()/2, b.cols()/2, b.cols());
+            Matrix b21 = b.partition(b.rows()/2, b.rows(), 0, b.cols()/2);
+            Matrix b22 = b.partition(b.rows()/2, b.rows(), b.cols()/2, b.cols());
+
+            Matrix c11 = new Matrix(a.rows()/2, a.cols()/2);
+            Matrix c12 = new Matrix(a.rows()/2, a.cols()/2);
+            Matrix c21 = new Matrix(a.rows()/2, a.cols()/2);
+            Matrix c22 = new Matrix(a.rows()/2, a.cols()/2);
+
+            // Step 2
+            Matrix s1 = subtract(b12, b22);
+            Matrix s2 = add(a11, a12);
+            Matrix s3 = add(a21, a22);
+            Matrix s4 = subtract(b21, b11);
+            Matrix s5 = add(a11, a22);
+            Matrix s6 = add(b11, b22);
+            Matrix s7 = subtract(a12, a22);
+            Matrix s8 = add(b21, b22);
+            Matrix s9 = subtract(a11, a21);
+            Matrix s10 = add(b11, b12);
+
+            // Step 3
+            Matrix p1 = multiplyStrassen(a11, s1);
+            Matrix p2 = multiplyStrassen(s2, b22);
+            Matrix p3 = multiplyStrassen(s3, b11);
+            Matrix p4 = multiplyStrassen(a22, s4);
+            Matrix p5 = multiplyStrassen(s5, s6);
+            Matrix p6 = multiplyStrassen(s7, s8);
+            Matrix p7 = multiplyStrassen(s9, s10);
+
+            // Step 4
+            c11 = add(subtract(add(add(c11, p5), p4), p2), p6);
+            c12 = add(add(c12, p1), p2);
+            c21 = add(add(c21, p3), p4);
+            c22 = subtract(subtract(add(add(c22, p5), p1), p3), p7);
+
+            c = new Matrix(c11, c12, c21, c22);
+
+        }
+
+        return c;
+
+    }
+
 }
